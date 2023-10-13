e5aef6195cd99c221ac79e6364e190de9108120c
just7z <huydao1021@yahoo.com>
2023-10-13 17:04:10 -0500

    section 16 -226

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -60,4 +60,10 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.shoppingListForm.reset();
     this.editMode = false;
   }
+
+  onDelete() {
+    this.shoppingListService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
+    this.editMode = false;
+  }
 }
