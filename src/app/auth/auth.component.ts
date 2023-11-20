19cbfa1ff9cb6e14f138cde27b301241784433d7
just7z <huydao1021@yahoo.com>
2023-11-20 11:26:49 -0600

    section 20 - 298

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -8,6 +8,8 @@ import {AuthService} from "./auth.service";
 })
 export class AuthComponent {
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
   constructor(private authService: AuthService) {
   }
@@ -23,17 +25,22 @@ export class AuthComponent {
     const email = form.value.email;
     const password = form.value.password;
 
+    this.isLoading = true;
     if (this.isLoginMode) {
       this.authService.login(email, password).subscribe(resData => {
         console.log(resData);
+        this.isLoading = false;
       }, error => {
-        console.log(error);
+        this.error = 'An error occured!';
+        this.isLoading = false;
       });
     } else {
       this.authService.signup(email, password).subscribe(resData => {
         console.log(resData);
+        this.isLoading = false;
       }, error => {
-        console.log(error);
+        this.error = 'An error occured!';
+        this.isLoading = false;
       });
     }
 
