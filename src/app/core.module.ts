d2518caae13015a80fdf81c706d18f4c36c7ba21
just7z <huydao1021@yahoo.com>
2024-01-03 09:32:20 -0600

    334

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -5,14 +5,14 @@ import { RecipeService } from "./recipes/recipe.service";
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
 
 @NgModule({
-    providers: [
-        ShoppingListService,
-        RecipeService,
-        {
-          provide: HTTP_INTERCEPTORS,
-          useClass: AuthInterceptorService,
-          multi: true
-        }
-    ]
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    }
+  ]
 })
 export class CoreModule {}
